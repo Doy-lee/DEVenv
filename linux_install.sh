96aaf3f6e43e0499c1969e7a5a3dae2cb637efa8
doyle <doyle.thai@aosgrp.us>
2020-11-05 14:41:47 +1100

    Update linux script

--- linux_install.sh
+++ linux_install.sh
@@ -2,40 +2,21 @@ if [[ ! -f "${HOME}/.vim/autoload/plug.vim" ]]; then
   wget --directory-prefix=../.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [[ ! -f "${HOME}/.vim/clang-format.py" ]]; then
-  wget --directory-prefix=${HOME}/.vim https://raw.githubusercontent.com/llvm-mirror/clang/master/tools/clang-format/clang-format.py
-fi
-
-cp -f Installer/_vimrc ~/.vimrc
-cp -f Installer/unix_nvim_init.vim ~/.config/nvim/init.vim
-
+#
+# Dependencies
+#
 sudo apt install exuberant-ctags neovim clang-format ripgrep
 
-cat > ~/.tmux.conf <<EOF
-set-option -g mouse on
-set-option escape-time 0
-set -g prefix2 C-a #Set prefix to also Ctrl+A
+#
+# NVim
+#
+cp --force Installer/_vimrc ~/.vimrc
+cp --force Installer/unix_nvim_init.vim ~/.config/nvim/init.vim
 
-# make scrolling with wheels work
-bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M' 'select-pane -t=; copy-mode -e; send-keys -M'"
-bind -n WheelDownPane select-pane -t= \; send-keys -M
-
-bind v split-window -v
-bind s split-window -h
-bind r source-file ~/.tmux.conf # Bind reload to R
-
-# switch panes using <prefix> (hjkl) vim style keys
-bind h select-pane -L
-bind l select-pane -R
-bind j select-pane -U
-bind k select-pane -D
-
-bind -n M-Left resize-pane -L 10
-bind -n M-Right resize-pane -R 10
-bind -n M-Up resize-pane -U 10
-bind -n M-Down resize-pane -D 10
-EOF
-
-mkdir -p Bin
-rm -rf Bin/ctags_cpp.sh
-echo ctags --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ $@ >> Bin/ctags_cpp.sh
+#
+# Ctags
+#
+mkdir --parents Bin
+rm --force Bin/ctags_cpp.sh
+echo ctags --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ \$\@ >> Bin/ctags_cpp.sh
+chmod +x Bin/ctags_cpp.sh
