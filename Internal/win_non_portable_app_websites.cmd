8ddf7a16233c5725cb818dad30a3969394a96720
doyle <doyle.github.2ifxi@simplelogin.com>
2023-02-07 22:01:39 +1100

    Add non-portable helper

---
+++ Internal/win_non_portable_app_websites.cmd
@@ -0,0 +1,10 @@
+@echo off
+start /B https://brave.com
+start /B https://desktop.telegram.org
+start /B https://www.ledger.com/ledger-live
+start /B https://getsession.org/windows
+start /B https://github.com/kapitainsky/RcloneBrowser/releases
+start /B https://www.mozilla.org/en-US/firefox/new
+start /B https://obsidian.md/download
+start /B https://protonvpn.com/download
+start /B https://signal.org/download
