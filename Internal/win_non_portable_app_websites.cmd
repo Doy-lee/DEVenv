740dd39f9460e3e2a5cbdf5dbf727695d657a2b3
doyle <doyle.thai@aosgrp.com>
2023-02-10 09:28:49 +1100

    Add spotify

--- Internal/win_non_portable_app_websites.cmd
+++ Internal/win_non_portable_app_websites.cmd
@@ -1,11 +1,12 @@
 @echo off
 start /B https://brave.com
 start /B https://desktop.telegram.org
-start /B https://www.ledger.com/ledger-live
+start /B https://ledger.com/ledger-live
 start /B https://getsession.org/windows
 start /B https://github.com/kapitainsky/RcloneBrowser/releases
-start /B https://www.mozilla.org/en-US/firefox/new
+start /B https://mozilla.org/en-US/firefox/new
 start /B https://obsidian.md/download
 start /B https://protonvpn.com/download
 start /B https://signal.org/download
 start /B https://safing.io
+start /B https://spotify.com/us/download
