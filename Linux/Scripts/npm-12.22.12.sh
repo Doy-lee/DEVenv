8326e730fb43e0f10122f7d53f2b92a1d9010154
doylet <git.doylet.6tnrz@simplelogin.com>
2024-02-05 13:38:19 +1100

    Add Linux scripts for calling into custom Node/NPM versions

---
+++ Linux/Scripts/npm-12.22.12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+desired_dir=${script_dir}/../NodeJS/12.22.12/bin
+desired_exe=${desired_dir}/npm
+PATH=${desired_dir}:${PATH}
+
+curr_script=$(realpath "${BASH_SOURCE[0]}")
+echo [DEVENVER] Executing script \"${curr_script}\" with \"${desired_exe}\"
+
+${desired_exe} $@
+
